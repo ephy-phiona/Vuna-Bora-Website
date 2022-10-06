5fb37bf9eeaa5b94c1a9d55acc51480754a4b824
ephy-phiona <ephyphiona@gmail.com>
2022-10-06 10:41:50 +0300

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import './navbar.css';
+
+function Navbar() {
+    return (
+      <div className="header">
+        
+        <nav class = "navbar">
+          <div class = "container">
+            <a href = "index.html" class = "navbar-brand">Vuna.Bora</a>
+            <div class = "navbar-nav">
+              <a href = "">Home</a>
+              <a href = "">About</a>
+              <a href = "">Product</a>
+              <a href = "">How it works</a>
+              <a href = "">Our Team</a>
+            </div>
+          </div>
+        </nav>
+        
+      </div>
+    );
+  }
+  export default Navbar;
+
+
+
+
+
+
